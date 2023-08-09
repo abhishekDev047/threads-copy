737004d5c8c2a3d77f9dfca85b21b75a654508d3
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-08-09 18:19:17 +0530

    app created and some comps done

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,45 @@
+import Topbar from '@/components/shared/Topbar';
+import '../globals.css';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Threads',
+  description: 'A simple Next.js 13 Threads copy app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+
+        <Topbar />
+
+        <LeftSidebar />
+
+        <section className='main-container'>
+            <div className="w-full w-max-4xl">
+              {children}
+            </div>
+        </section>
+
+        <RightSidebar />
+
+        <Bottombar />
+
+      </body>
+    </html>
+    </ClerkProvider>
+  )
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,21 @@
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <>
+
+    <main className="">
+      {/* <UserButton afterSignOutUrl="/"/> */}
+      <h1 className="head-text text-left "> Home </h1>
+    </main>
+    
+  </>
+  )
+};
+
+
+    
+     
+    
+  
